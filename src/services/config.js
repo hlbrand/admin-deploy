8befeaec0bd65ab7ecd1310ce3dbd6d44fa8729e
hlbrand <116349848+hlbrand@users.noreply.github.com>
2022-12-27 16:14:31 +0700

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,3 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://precious-mandazi-06a722.netlify.app"
+const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://medusa-deploy-production.up.railway.app"
 
 export { medusaUrl }
